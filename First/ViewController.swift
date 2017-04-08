f97915ae991b7b899d99892bfa4d10f1d6174a01
Mingun Pak <mingunpa@usc.edu>
2017-04-07 23:57:44 -0700

    done: chapter 2

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //textfield.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func buttonWasClicked(_ sender: Any) {
+        label.text = "Hi \(textfield.text!)!"
+        textfield.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.textfield.resignFirstResponder()
+        return false
     }
 
     override func didReceiveMemoryWarning() {
